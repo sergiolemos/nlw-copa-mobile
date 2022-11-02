ebd6fcffe4341268bfd76a6ffc9f7c2b0a267156
Sérgio Lemos <contato@sergiolemos.com>
2022-11-02 20:43:02 -0300

    import repository

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function Signin() {
+    return (
+        <Center flex={1} bgColor="black">
+            <Text color="gray.900" fontSize={48}>
+                Sign In
+            </Text>
+        </Center>
+    )
+}
